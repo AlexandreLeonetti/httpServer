e0282e4a91240a6f5df5ea20d438368758373451
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-05-30 22:12:13 +0200

    serverPart2

--- basicHttpServer/src/main/java/basicHttpServer/basicHttpServer/HttpServerApp.java
+++ basicHttpServer/src/main/java/basicHttpServer/basicHttpServer/HttpServerApp.java
@@ -1,8 +1,12 @@
 package basicHttpServer.basicHttpServer ;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-
+import java.util.List;
+import java.util.Map;
 public  class HttpServerApp{
 	public static void main(String[] args) throws Exception {
 		var serverSocket = new ServerSocket(8080);
@@ -10,6 +14,8 @@ public  class HttpServerApp{
 				System.out.println("server listenning on 8080");
 		while(true) {
 			var connection = serverSocket.accept();
+			var request    = readRequest(connection);
+			System.out.println("we have a new request : "+request);
 			try( var os = connection.getOutputStream()){
 				String body = """
 						{
@@ -29,7 +35,23 @@ public  class HttpServerApp{
 				System.out.println("server listenning on 8080");
 			}
 		}
+	}
+	private static HttpReq readRequest(Socket connection) throws Exception {//7m49
+		var r = new BufferedReader(new InputStreamReader(connection.getInputStream())); 
+			var line = r.readLine();
+			var methodUrl = line.split(" ");
+			var method	=methodUrl[0];
+			var url =  methodUrl[1];
+			var protocol = methodUrl[2];
+			System.out.println("request");
+			return new HttpReq(method, url, protocol,  Map.of(),null);
 		
+	}
+	private record HttpReq(String method,
+			String url,
+			String protocol,
+			Map<String, List<String>> headers,
+			Byte[] body) {
 		
 	}
 }
